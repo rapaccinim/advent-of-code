f03e37d17222be7fa2235a28e13632ab7938f0cd
Marco Rapaccini <info@marcorapaccini.it>
2023-12-02 16:53:38 +0000

    First part completed

--- 2023/day-2/part1.rb
+++ 2023/day-2/part1.rb
@@ -10,20 +10,8 @@ GAME_REGEX = /[^:]*$/
 
 sum = 0
 
-def get_cubes(set)
-  set.split(',')
-end
-
 def get_values(cube)
   cube.split(/(\D+)/)
-end
-
-# def get_hash(values)
-#   [values[1], values[0].to_i].to_h
-# end
-
-def get_sets(game)
-  game.split(/;/).map!{|item| item.delete(" \t\r\n")  }
 end
 
 def get_hash(cubes)
@@ -33,31 +21,30 @@ def get_hash(cubes)
   end).to_h
 end
 
+def get_sets(game)
+  game.split(/;/).map!{|item| item.delete(" \t\r\n")  }
+end
+
 def get_hashes(sets)
-  sets.map! do |set|
-    get_hash(get_cubes(set))
-  end
+  sets.map! { |set| get_hash(get_cubes(set)) }
+end
+
+def get_cubes(set)
+  set.split(',')
 end
 
 def is_hash_within_max_values(hash)
-  puts "Hash: #{hash}"
+  (MAX_VALUES.keys & hash.keys).all? { |key| hash[key] <= MAX_VALUES[key]}
 end
 
 def is_game_within_max_values(hashes)
-  # it will be a all?
-  puts "Hashes: #{hashes}"
-  hashes.each do
-    is_hash_within_max_values(hash)
-  end
+  hashes.all?(&method(:is_hash_within_max_values))
 end
 
 File.open(FILE_LOCATION).each_with_index do |line, index|
   game = line[GAME_REGEX]
-  puts "Each game"
-  puts game
   sets = get_sets(game)
-  puts sets.to_s
   hashes = get_hashes(sets)
-  is_game_within_max_values(hashes)
+  sum+= index + 1 if is_game_within_max_values(hashes)
 end
 puts "The sum of all of the calibration values is: #{sum}"
